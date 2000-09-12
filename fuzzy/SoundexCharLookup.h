fba8c337e0f020fcb9fa3a093892dbac19474bd6
John Anderson <panic@semiosix.com>
2000-09-12 13:11:30 +0000

    Make the charmap separate to Soundex
    git-svn-id: file:///srv/svnroot/semilib/trunk@1693 a19b6091-82f5-0310-b5c5-f4b00232789b

---
+++ fuzzy/SoundexCharLookup.h
@@ -0,0 +1,17 @@
+#ifndef SoundexCharLookup_h
+#define SoundexCharLookup_h
+/*
+**			  Group   Letters
+**			  1			   BFPV
+**			  2			   CGJKSXZ
+**			  3			   DT
+**			  4			   L
+**			  5			   MN
+**			  6			   R
+*/
+
+namespace soundex
+{
+	extern char * lookup;
+}
+#endif
