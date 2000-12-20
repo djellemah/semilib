7236a474599edefb6ee5087563b7154907bc0af8
John Anderson <panic@semiosix.com>
2000-12-20 09:44:39 +0000

    Add more tests Make sure they all output something
    git-svn-id: file:///srv/svnroot/semilib/trunk@1771 a19b6091-82f5-0310-b5c5-f4b00232789b

--- utils/test.cpp
+++ utils/test.cpp
@@ -16,7 +16,13 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
+#ifdef _MSC_VER
+// ignore debug info truncated messages
 #pragma warning(disable: 4786)
+
+// ignore "c++ exception specification ignored" messages
+#pragma warning(disable: 4290)
+#endif
 
 #include <iostream>
 #include <sstream>
@@ -33,6 +39,7 @@ using namespace std;
 #include "FileUtils.h"
 #include "functions.h"
 #include "SmartPointer.h"
+#include "Result.h"
 #include "TempFile.h"
 
 using namespace Utils;
@@ -42,6 +49,11 @@ using std::endl;
 using std::string;
 using std::istringstream;
 
+/******************************************
+	FlagsMapper - convert between bitmapped
+	integers and option names. Mostly for
+	debugging
+******************************************/
 #include "FlagsMapper.h"
 
 int test_FlagsMapper ( int argc, char * argv[] )
@@ -57,11 +69,13 @@ int test_FlagsMapper ( int argc, char * argv[] )
 	FlagsMapper fm ( flagsString );
 	try
 	{
+		cout << "Should be 8" << endl;
 		cout << fm["REG_NOSUB"] << endl;
 		for ( int i = 1; i < 16; ++i )
 		{
 			cout << fm[i] << endl;
 		}
+		cout << "Should be 9" << endl;
 		cout << fm[" REG_NOSUB|REG_EXTENDED "] << endl;
 	}
 	catch ( exception & e )
@@ -71,6 +85,13 @@ int test_FlagsMapper ( int argc, char * argv[] )
 	
 	return 0;
 }
+
+/******************************************
+	A memory allocation algorithm that's faster
+	and uses less memory. From Dr Dobbs originally.
+	
+	Look in smartnew.cpp for the implementation
+******************************************/
 
 inline int calc_alloc_size(int max_alloc_size)
 {
@@ -143,6 +164,10 @@ int test_Smartnew(int argc, char* argv[])
 	return 0;
 }
 
+/******************************************
+	Test that MSVC dll linkage with the libraries
+	works OK.
+******************************************/
 int test_DllLink ( int argc, char * argv[] )
 {
 	vector<string> parts = splitPath( argv[0] );
@@ -162,19 +187,30 @@ int test_DllLink ( int argc, char * argv[] )
 	return 0;
 }
 
+/******************************************
+******************************************/
+
 int test_SmartPointer ( int argc, char * argv[] )
 {
 	SmartPointer<char> p = new char[256];
 	SmartPointer<char> q = p;
+
+	// actually, two assignments in a row like this
+	// cause ownership to be lost...
 	p = q;
 	p = q;
 
-	strcpy ( p, "hello" );
-	cout << toUpper ( p ) << endl;
+	strcpy ( p, "the same" );
+
+	cout << "These two lines should be the same" << endl;
 	cout << p << endl;
+	cout << q << endl;
 
-	return 0;
+	return strcmp ( p, q ) == 0 ? 0 : 1;
 }
+
+/******************************************
+******************************************/
 
 #ifdef _WIN32
 int test_ssprintf ( int argc, char * argv[] )
@@ -185,17 +221,24 @@ int test_ssprintf ( int argc, char * argv[] )
 }
 #endif
 
-/**************************
+/******************************************
 Test persistence manager
-**************************/
+******************************************/
 
+/*
+	A little baby class to test the persistence framework
+*/
 class TestPersist
 {
 public:
+	
+	// required for persistence
 	TestPersist( int value = 0 )
 	{
 		_value = value;
 	}
+	
+	// 'value' accessors
 	const int value() const
 	{
 		return _value;
@@ -207,6 +250,7 @@ public:
 		return *this;
 	}
 
+	// for easy testing of the restored object
 	bool operator == ( const TestPersist & other )
 	{
 		return value() == other.value();
@@ -219,6 +263,8 @@ public:
 
 private:
 	int _value;
+	
+	// required for persistence
 	friend ostream & operator << ( ostream &, const TestPersist & );
 	friend istream & operator >> ( istream &, TestPersist & );
 };
@@ -251,54 +297,55 @@ namespace {
 	Constructor<TestPersist> b;
 }
 
-int test_Persistence ( int argc, char * argv[] )
+int test_Persistence ( int argc, char * argv[] ) throw ( runtime_error )
 {
-	try
+	TestPersist p ( 5 );
+	PersistenceManager<TestPersist> pm;
+	bool result = true;
+	
+	// test with stringstreams
 	{
+		cout << "Testing with stringstreams"<< endl;
+		
 		// make it persistent
-		TestPersist p ( 5 );
-		PersistenceManager<TestPersist> pm;
-		bool result = true;
+		ostringstream ostr;
+		pm.persist ( ostr, p );
 		
-		{
-			cout << "Testing with stringstreams"<< endl;
-			ostringstream ostr;
-			pm.persist ( ostr, p );
-			
-			// restore it
-			istringstream istr ( ostr.str() );
-			SmartPointer<TestPersist> ptr = pm.restore ( istr );
+		// restore it
+		istringstream istr ( ostr.str() );
+		SmartPointer<TestPersist> ptr = pm.restore ( istr );
 
-			// results
-			cout << "Original: " << p.value() << endl;
-			cout << "Restored: " << ptr->value() << endl;
-			result = result && ( p == *ptr );
-		}
-
-		{
-			cout << "Testing with iostreams"<< endl;
-			TempFile tempfile;
-			ofstream ofs ( tempfile );
-			pm.persist ( ofs, p );
-			ofs.flush();
-			
-			ifstream ifs ( tempfile );
-			SmartPointer<TestPersist> ptr = pm.restore ( ifs );
-			cout << "Original: " << p.value() << endl;
-			cout << "Restored: " << ptr->value() << endl;
-			
-			result = result && ( p == *ptr );
-		}
-		
-		return result ? 0 : -1;
+		// results
+		cout << "Original: " << p.value() << endl;
+		cout << "Restored: " << ptr->value() << endl;
+		result = result && ( p == *ptr );
 	}
-	catch ( exception & e )
+	
+	// test with fstreams
 	{
-		cout << e.what() << endl;
-		return -1;
+		cout << "Testing with iostreams"<< endl;
+		
+		// make it persistent
+		TempFile tempfile;
+		ofstream ofs ( tempfile );
+		pm.persist ( ofs, p );
+		
+		// shouldn't really be necessary in a line-based persistence,
+		// but put it here to be safe
+		ofs.flush();
+		
+		// restore it
+		ifstream ifs ( tempfile );
+		SmartPointer<TestPersist> ptr = pm.restore ( ifs );
+		
+		// results
+		cout << "Original: " << p.value() << endl;
+		cout << "Restored: " << ptr->value() << endl;
+		
+		result = result && ( p == *ptr );
 	}
-
-	return 0;
+	
+	return result ? 0 : -1;
 }
 
 #ifdef _WIN32
@@ -307,23 +354,155 @@ int test_Persistence ( int argc, char * argv[] )
 	#define TEMPDIR "/tmp"
 #endif
 
+/******************************************
+	Test TempFile
+******************************************/
 
 #include "TempFile.h"
 
-int test_TempFile ( int argc, char * argv[] )
+int test_TempFile ( int argc, char * argv[] ) throw ( runtime_error )
 {
+	cout << "Testing TempFile" << endl;
+	// test in local directory with generated name
 	TempFile t1;
-	TempFile t2 ( TEMPDIR );
+	cout << "Create temporary filename: " << t1.asString();
+	if ( FileUtils::exists ( t1.asString() ) )
+		throw runtime_error ( "file " + t1.asString() + " already exists" );
+	cout << " OK" << endl;
+
+	// test in local directory with supplied prefix
+	TempFile t3 ( "bgot" );
+	cout << "Create temporary filename: " << t3.asString();
+	if ( FileUtils::exists ( t3.asString() ) )
+		throw runtime_error ( "file " + t3.asString() + " already exists" );
+	cout << " OK" << endl;
+
+	// test with a specified directory
+	TempFile t2 ( "tmp", TEMPDIR );
+	cout << "Create temporary filename: " << t3.asString();
+	if ( FileUtils::exists ( t2.asString() ) )
+		throw runtime_error ( "file " + t1.asString() + " already exists" );
+	cout << " OK" << endl;
 
 	return 0;
 }
 
+/******************************************
+	Test mkdir
+******************************************/
+
 int test_mkdir ( int argc, char * argv[] )
 {
-	Utils::mkdir ( string ( TEMPDIR ) + directorySeparator + "dirone" + directorySeparator + "dirtwo" );
-	Utils::mkdir ( string ( "dirone" ) + directorySeparator + "dirtwo" );
-	Utils::mkdir ( string ( "." ) + directorySeparator + "dirdotone" + directorySeparator + "dirtwo" );
+	cout << "Testing mkdir" << endl;
+	string temp;
+
+	temp = string ( TEMPDIR ) + directorySeparator + "dirone" + directorySeparator + "dirtwo";
+	cout << "Create " << temp;
+	Utils::mkdir ( temp );
+	if ( !FileUtils::exists ( temp ) )
+		throw runtime_error ( "directory " + temp + " not created" );
+	cout << "OK" << endl;
+
+	temp = string ( "dirone" ) + directorySeparator + "dirtwo";
+	cout << "Create " << temp;
+	Utils::mkdir ( temp );
+	if ( !FileUtils::exists ( temp ) )
+		throw runtime_error ( "directory " + temp + " not created" );
+	cout << "OK" << endl;
+	
+	temp = string ( "dirdotone" ) + directorySeparator + "dirtwo";
+	cout << "Create " << temp;
+	Utils::mkdir ( temp );
+	if ( !FileUtils::exists ( temp ) )
+		throw runtime_error ( "directory " + temp + " not created" );
+	cout << "OK" << endl;
+
 	return 0;
+}
+
+/******************************************
+	Test istring
+******************************************/
+
+#include "istring.h"
+
+bool search ( const string & tofind, const istring & in )
+{
+	bool result = in.find ( istring ( tofind ) ) != string::npos;
+	cout
+		<< boolalpha << result
+		<< ": " << "Case-insensitive search for "
+		<< tofind
+		<< " in "
+		<< '"' << in << '"'
+		<< endl
+	;
+	return result;
+}
+
+int test_istring ( int argc, char * argv[] )
+{
+	istring query( "select count (*) from pcodes;" );
+	bool result = true;
+	
+	cout << "Should be true" << endl;
+	result = result && search ( "count", query );
+	
+	cout << "Should be true" << endl;
+	result = result && search ( "Count", query );
+	
+	cout << "Should be true" << endl;
+	result = result && search ( "COUNT", query );
+	
+	cout << "Should be false" << endl;
+
+	// NOTE ---------- !
+	//                 |
+	//                 v
+	result = result && !search ( "blount", query );
+	
+	return result ? 0 : -1;
+}
+
+/******************************************
+	Test file existence
+******************************************/
+int test_exists ( int argc, char * argv[] )
+{
+	// test file utils
+	
+	string filename = argv[0];
+
+	{
+		cout << "Testing for file: " << filename << endl;
+		FileUtils fu ( filename );
+		cout << boolalpha << fu.exists() << endl;
+		cout << fu.reason() << endl;
+		cout << boolalpha << FileUtils::exists ( filename ) << endl;
+	}
+
+	filename ="somedumfilename";
+	{
+		FileUtils fu ( filename );
+		cout << "Testing for file: " << filename << endl;
+		cout << boolalpha << fu.exists() << endl;
+		cout << fu.reason() << endl;
+		cout << boolalpha << FileUtils::exists ( filename ) << endl;
+	}
+
+	cout << "Should get 'No filename specified' exception" << endl;
+	try
+	{
+		cout << boolalpha << FileUtils::exists("") << endl;
+	}
+	catch ( exception & e )
+	{
+		cout << "Exception caught: " << e.what() << endl;
+		cout << "Next" << endl;
+		return 0;
+	}
+
+	return 1;
 }
 
 /*
@@ -331,20 +510,50 @@ int test_mkdir ( int argc, char * argv[] )
 */
 int main ( int argc, char * argv[] )
 {
+	// so gdb has somewhere to stop
 	cout << "Starting" << endl;
 	
+	// this will throw an exception if one of the tests
+	// returns a nonzero result
+	Result<int> result;
+
 	try
 	{
-		return test_Persistence ( argc, argv );
+#if 0
+		// don't use this much
+		result = test_Smartnew(argc, char* argv);
+#endif
+
+#ifdef _WIN32
+		result = test_DllLink ( argc, argv );
+		result = test_ssprintf ( argc, argv );
+#endif
+		cout << "Test at " << __LINE__ << endl;
+		result = test_exists ( argc, argv );
+		cout << "Test at " << __LINE__ << endl;
+		result = test_TempFile ( argc, argv );
+		cout << "Test at " << __LINE__ << endl;
+		result = test_FlagsMapper ( argc, argv );
+		cout << "Test at " << __LINE__ << endl;
+		result = test_SmartPointer ( argc, argv );
+		cout << "Test at " << __LINE__ << endl;
+		result = test_Persistence ( argc, argv );
+		cout << "Test at " << __LINE__ << endl;
+		result = test_mkdir ( argc, argv );
+		cout << "Test at " << __LINE__ << endl;
+		result = test_istring ( argc, argv );
+
+		cout << "All testes [sic] successful" << endl;
 	}
 	catch ( exception & e )
 	{
 		cout << e.what() << endl;
+		return result;
 	}
 	catch ( ... )
 	{
 		cout << "Caught ..." << endl;
+		return result;
 	}
-	
-	return 1;
+	return result;
 }
