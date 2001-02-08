576b1fea5b6206d7c6ddeaf062ab378598638243
John Anderson <panic@semiosix.com>
2001-02-08 18:19:01 +0000

    Minor ifdef changes
    git-svn-id: file:///srv/svnroot/semilib/trunk@1788 a19b6091-82f5-0310-b5c5-f4b00232789b

--- fuzzy/fuzzydlldef.h
+++ fuzzy/fuzzydlldef.h
@@ -19,7 +19,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef fuzzydlldef_h
 #define fuzzydlldef_h
 
-#ifdef WIN32
+#ifdef _WIN32
 	#ifdef FUZZY_NO_USE_DLL
 		#define FUZZY_DLL_API
 	#endif
