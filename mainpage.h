000c71025d80539e52e173f00aaf316dcad2a1c7
John Anderson <panic@semiosix.com>
2002-03-25 08:23:11 +0000

    some introductory docunmentatio
    git-svn-id: file:///srv/svnroot/semilib/trunk@1829 a19b6091-82f5-0310-b5c5-f4b00232789b

---
+++ mainpage.h
@@ -0,0 +1,14 @@
+/**
+/\mainpage
+This is the Semiosix library of Useful c++ Things (tm). It contains
+various utility classes:
+	- Regex posix regex wrapper
+	- SmartPointer
+	- RefCount reference counted smart pointer
+	- soundex and ngram matching
+	- featherweight persistence framework
+	- temporary file object
+	- various file handling objects and methods
+	- function error code return value object (Result)
+	- FlagsMapper maps a set of ints (usually defines) to string error messages
+*/
