41ac6aaec0f15d258acad712f76842e7a6034346
John Anderson <panic@semiosix.com>
2002-09-09 19:47:48 +0000

    comments
    git-svn-id: file:///srv/svnroot/semilib/trunk@1860 a19b6091-82f5-0310-b5c5-f4b00232789b

--- utils/Mutex.h
+++ utils/Mutex.h
@@ -8,20 +8,17 @@
 #endif
 
 /**
-	Mutexhronisation class. Create on of these whenever a critical section is
+	Mutex class. Create on of these whenever a critical section is
 	necessary. Then, on entering the critical section do:
-	<pre>
-		Mutex aMutex ( mutex );
-	</pre>
-	the Mutex will be released when aMutex goes out of scope.
+	\code
+		Lock lock ( mutex );
+	\endcode
+	the Mutex will be released when lock goes out of scope.
 
-	The Mutex object should be in a scope outside that of aMutex. A good
+	The Mutex object should be in a scope outside that of lock. A good
 	way to do it is to make Mutex class member, and then for each
 	method that needs to control access, have the above line as the
 	first line in the method implementation.
-
-	\todo this is a _WIN32 specific class, but the functionality it uses
-	should be available in any thread implementation. So, implement it, already!
 */
 class Mutex
 {
