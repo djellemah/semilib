e460b42508639a2e4febee2ec17a84338df5681b
John Anderson <panic@semiosix.com>
2003-02-17 20:54:33 +0000

    output to cerr instead
    git-svn-id: file:///srv/svnroot/semilib/trunk@1916 a19b6091-82f5-0310-b5c5-f4b00232789b

--- utils/CoutLogger.cpp
+++ utils/CoutLogger.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 /**
-	The default logger just sends to stdout
+	The default logger just sends to stderr
 */
 class CoutLogger : public Logger
 {
 public:
 	void doLog ( const string & msg, Level::LogLevel level )
 	{
-		cout << level << ": " << msg << endl;
+		cerr << level << ": " << msg << endl;
 	}
 };
 
