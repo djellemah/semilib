b31de66f5944b3a18245e622ee2b8f6e8c2e3729
John Anderson <panic@semiosix.com>
2003-05-10 20:28:10 +0000

    try with the same data that crashes in PerformanceFactory
    git-svn-id: file:///srv/svnroot/semilib/trunk@1949 a19b6091-82f5-0310-b5c5-f4b00232789b

--- utils/testJoin.cpp
+++ utils/testJoin.cpp
@@ -8,11 +8,20 @@ using namespace std;
 int main ( int argc, char * argv[] )
 {
 	vector<string> values;
+	values.push_back ( "20030510" );
+	values.push_back ( "20030510" );
+/*
 	values.push_back ( "one" );
 	values.push_back ( "two" );
 	values.push_back ( "three" );
-	
-	cout << join ( values, argc ) << endl;
+	values.push_back ( "three" );
+	values.push_back ( "three" );
+	values.push_back ( "three" );
+	values.push_back ( "three" );
+	values.push_back ( "three" );
+*/
+
+	cout << join ( values, "," ) << endl;
 	
 	return 0;
 }
