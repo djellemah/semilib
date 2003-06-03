abd0055da36aa99e7c4e4cc953a368720f95edc8
John Anderson <panic@semiosix.com>
2003-06-03 15:48:30 +0000

    fix refcount
    git-svn-id: file:///srv/svnroot/semilib/trunk@1968 a19b6091-82f5-0310-b5c5-f4b00232789b

---
+++ utils/testRefcount.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+#include "RefCount.h"
+
+using namespace std;
+
+int main()
+{
+	long * ptr = new long;
+	*ptr = 12345;
+	RefCount<long> value = ptr;
+	{
+		RefCount<long> copy = value;
+		cout << "copy: " << value << endl;
+	}
+	cout << "value: " << value << endl;
+}
