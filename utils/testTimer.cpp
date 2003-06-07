ad3fd760842d369ada00c23f574e24f17d421fa6
John Anderson <panic@semiosix.com>
2003-06-07 12:09:45 +0000

    test timer resolution for win32
    git-svn-id: file:///srv/svnroot/semilib/trunk@1974 a19b6091-82f5-0310-b5c5-f4b00232789b

--- utils/testTimer.cpp
+++ utils/testTimer.cpp
@@ -1,4 +1,9 @@
+#ifndef _WIN32
 #include <unistd.h>
+#else
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
 
 #include "utils.h"
 #include "Timer.h"
@@ -11,7 +16,11 @@ using namespace std;
 int main ( int argc, char * argv[] )
 {
 	Timer timer;
+#ifndef _WIN32
 	sleep ( 5 );
+#else
+	Sleep ( 5000 );
+#endif
 	cout << "elapsed: " << timer.elapsed() << endl;
 	cout << "stop: " << timer.stop() << endl;
 }
