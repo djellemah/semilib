fa98e049ddd8ca5fb133e201b2de24d0a519f8da
John Anderson <panic@semiosix.com>
2003-11-30 15:47:02 +0000

    make shutdown-safe, use mutexes more
    git-svn-id: file:///srv/svnroot/semilib/trunk@1989 a19b6091-82f5-0310-b5c5-f4b00232789b

--- utils/Singleton.cpp
+++ utils/Singleton.cpp
@@ -21,27 +21,26 @@
 #include <map>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+
+#include <semilib/utils.h>
 
 using namespace std;
-
-/**
-	This keeps the order in which singletons were constructed
-	so that we can destroy them in reverse order
-*/
-typedef vector<string> InstanceOrder;
-InstanceOrder instanceOrder;
-
 
 /**
 	This is a global set of all singleton instances. DO NOT
 	declare this as DLL exportable, or allow it to escape
 	from it's DLL.
 */
-Mutex instancesMutex;
+Mutex * _instancesMutex = 0;
 
 Mutex & getMutex()
 {
-	return instancesMutex;
+	if ( _instancesMutex == 0 )
+	{
+		_instancesMutex = new Mutex();
+	}
+	return *_instancesMutex;
 }
 
 /**
@@ -50,34 +49,76 @@ Mutex & getMutex()
 */
 class Instances : public map<string, SingletonBase*>
 {
+	/**
+		This keeps the order in which singletons were constructed
+		so that we can destroy them in reverse order
+	*/
+	typedef vector<string> InstanceOrder;
+	InstanceOrder instanceOrder;
+
 public:
 	Instances()
 	{
 	}
 
-	void cleanup()
+	void remove ( const std::string & name )
 	{
-		Lock lock ( instancesMutex );
-		while ( instanceOrder.size() > 0 )
+		Lock lock ( getMutex() );
+
+		// remove the instance order entry
+		//cout << "Searching for " << name << " in " << join ( instanceOrder ) << endl;
+		InstanceOrder::iterator orderit = std::find ( instanceOrder.begin(), instanceOrder.end(), name );
+		if ( orderit != instanceOrder.end() )
 		{
-			string name = instanceOrder.back();
-			instanceOrder.pop_back();
+			instanceOrder.erase ( orderit );
 			iterator it = find ( name );
 			if ( it != end() )
 			{
 				SingletonBase * base = it->second;
-				delete base;
 				erase ( it );
 			}
 		}
 	}
 
+	
+	void add ( const std::string & name, SingletonBase * instance )
+	{
+		cout << "Adding " << name << " at " << instance << endl;
+		instanceOrder.push_back ( name );
+		operator[] ( name ) = instance;
+	}
+
 	/**
 		Clean up singletons in reverse order of construction
 	*/
+	void cleanup()
+	{
+		Lock lock ( getMutex() );
+		while ( instanceOrder.size() > 0 )
+		{
+			string name = instanceOrder.back();
+			cout << "Attempting " << name << "cleanup" << endl;
+			instanceOrder.pop_back();
+			iterator it = find ( name );
+			if ( it != end() )
+			{
+			cout << "Found " << name << endl;
+				SingletonBase * base = it->second;
+			cout << "Erasing " << name << endl;
+				erase ( it );
+			cout << "deleting " << name << " at " << base << endl;
+				delete base;
+			cout << "Finished " << name << endl;
+			}
+			cout << "Finished " << name << "cleanup" << endl;
+		}
+	}
+
 	~Instances()
 	{
+		cout << "semilib::~Instances" << endl;
 		cleanup();
+		cout << "finished semilib::~Instances" << endl;
 	}
 };
 
@@ -86,37 +127,113 @@ public:
 	declare this as DLL exportable, or allow it to escape
 	from its DLL.
 */
-Instances instances;
+Instances * _instances = 0;
+
+Instances & getInstances()
+{
+	if ( _instances == 0 )
+	{
+		_instances = new Instances();
+	}
+	return *_instances;
+}
 
 bool haveInstance ( const string & name )
 {
-	return instances.find ( name ) != instances.end();
+	Lock lock ( getMutex() );
+	return getInstances().find ( name ) != getInstances().end();
 }
 
 void keepInstance ( const std::string & name, SingletonBase * instance )
 {
-	instanceOrder.push_back ( name );
-	instances[name] = instance;
+	getInstances().add ( name, instance );
+}
+
+void removeInstance ( const string & name )
+{
+	cout << "remove instance " << name << endl;
+	getInstances().remove ( name );
+	cout << "finished remove instance " << name << endl;
 }
 
 SingletonBase * getInstance ( const std::string & name )
 {
-	return instances[name];
+	Lock lock ( getMutex() );
+	return getInstances()[name];
 }
 
 void acquireLock()
 {
-	instancesMutex.lock();
+	getMutex().lock();
 }
 
 void releaseLock()
 {
-	instancesMutex.release();
+	getMutex().release();
 }
 
 void deleteSingletons()
 {
-	instances.cleanup();
+	getInstances().cleanup();
 }
+
+class SingletonInit
+{
+public:
+	SingletonInit()
+	{
+		if ( _instancesMutex == 0 )
+		{
+			_instancesMutex = new Mutex();
+		}
+		if ( _instances == 0 )
+		{
+			_instances = new Instances();
+		}
+	}
+
+	~SingletonInit()
+	{
+		// reverse order of construction
+		delete _instances;
+		delete _instancesMutex;
+	}
+};
+
+namespace {
+	SingletonInit init;
+}
+
+#if 0
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+BOOL APIENTRY DllMain(
+	HANDLE hModule
+	, DWORD  ul_reason_for_call
+	, LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+		case DLL_PROCESS_ATTACH:
+			cout << "Loading semilib dll" << endl;
+			break;
+		case DLL_THREAD_ATTACH:
+			cout << "attach thread semilib dll" << endl;
+			break;
+		case DLL_THREAD_DETACH:
+			cout << "detach thread semilib dll" << endl;
+			break;
+		case DLL_PROCESS_DETACH:
+			cout << "Unloading semilib dll" << endl;
+			break;
+	}
+	return TRUE;
+}
+#endif // _WIN32
+
+#endif // if 0
 
 #endif
