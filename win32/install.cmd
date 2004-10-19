1d1f6b3e78883b0a7b45850e97c17f9abc879178
John Anderson <panic@semiosix.com>
2004-10-19 07:36:52 +0000

    we don't have fuzzy anymore
    git-svn-id: file:///srv/svnroot/semilib/trunk@2023 a19b6091-82f5-0310-b5c5-f4b00232789b

--- win32/install.cmd
+++ win32/install.cmd
@@ -2,11 +2,7 @@ mkdir ..\..\include
 mkdir ..\..\include\semilib
 rem del /q ..\..\include\semilib\*
 rem copy ..\utils\*.h ..\..\include\semilib
-rem copy ..\fuzzy\*.h ..\..\include\semilib
 rem copy ..\regex\*.h ..\..\include\semilib
 
-echo %path%
-
 call rsync -vt --delete ../utils/*.h ../../include/semilib
-call rsync -vt --delete ../fuzzy/*.h ../../include/semilib
 call rsync -vt --delete ../regex/*.h ../../include/semilib
